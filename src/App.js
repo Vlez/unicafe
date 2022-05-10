950397cf101c6ebc0e700bfa4a95d1a491f45c44
Gases <aariasmartijena@gmail.com>
2022-05-10 17:21:47 -0400

    complete exercises 1.6-1.11

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = ((good - bad) / all).toFixed(2);
+  const positive = `${((good * 100) / all).toFixed(2)}%`;
+
+  if (all) {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <strong>good</strong>
+            </td>
+            <td>{good}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>neutral</strong>
+            </td>
+            <td>{neutral}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>bad</strong>
+            </td>
+            <td>{bad}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>all</strong>
+            </td>
+            <td>{all}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>average</strong>
+            </td>
+            <td>{average}</td>
+          </tr>
+          <tr>
+            <td>
+              <strong>positive</strong>
+            </td>
+            <td>{positive}</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+
+  return <div>no feedback given</div>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => {
+    setGood(good + 1);
+  };
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleBad = () => {
+    setBad(bad + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <br />
+      <h1>statistics</h1>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
-}
-
+};
 export default App;
